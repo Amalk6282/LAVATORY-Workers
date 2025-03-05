9dc64c56921498002a35e4360f74106417d03694
akash <akashrajark@gmail.com>
2025-03-05 22:37:15 +0530

    complete base

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:lacatory_workers/login_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Supabase.initialize(
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0eGRqbnZibW9obmhzdnBsaGZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI4NTczNTUsImV4cCI6MjA0ODQzMzM1NX0.DJoD6XNa-GIyeVmDcMbgoxXL4OeNtozBUIpnf185mTg',
+    url: 'https://wtxdjnvbmohnhsvplhft.supabase.co',
+  );
+
   runApp(const MyApp());
 }
 
